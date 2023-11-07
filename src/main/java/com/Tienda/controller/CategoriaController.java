c92e2de24c1d26f2d4b25a3621980ada2ff1af38
Dylan <Dylan@192.168.1.68>
2023-11-07 15:21:41 -0600

    Cambios semana 8

--- src/main/java/com/Tienda/controller/CategoriaController.java
+++ src/main/java/com/Tienda/controller/CategoriaController.java
@@ -5,7 +5,6 @@ import com.Tienda.service.CategoriaService;
 import com.Tienda.service.impl.FirebaseStorageServiceImpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/Tienda/domain/Categoria.java
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -15,6 +15,7 @@ public class Categoria implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Categoria")
     private Long idCategoria;
     private String descripcion;
     private String rutaImagen;
